37ae6a4e954e2e97d8f745fa34694e5a5f1f6d88
kalyanlimkar-3 <kalyan.limkar@gmail.com>
2025-02-10 01:33:56 +0530

    Binary search: Adding algorithm pseudo code

--- cmd/search/binary/binary.go
+++ cmd/search/binary/binary.go
@@ -1,3 +1,16 @@
+/* Binary Search
+
+Algorithm:
+	while (there are more integers to check)
+		middle = (left + right) / 2;
+		if searchnum == arr[middle]:
+			return middle;
+		else if arr[middle] < searchnum:
+			left = middle + 1;
+		else:
+			right = middle - 1;
+*/
+
 package main
 
 import (
@@ -12,11 +25,11 @@ func main() {
 		searchItem int   = 4
 	)
 
-	fmt.Println("Input array: ", arr, ", Search item: ", searchItem)
+	fmt.Println("Input array:", arr, ", Search item:", searchItem)
 	found := search.Binary(arr, searchItem)
 	if found == -1 {
 		fmt.Println("Not found")
 	} else {
-		fmt.Println("Found at index: ", found)
+		fmt.Println("Found at index:", found)
 	}
 }
