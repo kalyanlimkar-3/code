80a3b39dd21b36a9da11659c04f6dc60336642e9
kalyanlimkar-3 <kalyan.limkar@gmail.com>
2025-03-17 23:57:33 +0530

    Multiplication of 2 matrices

--- internal/matrix/matrix.go
+++ internal/matrix/matrix.go
@@ -10,7 +10,7 @@ var (
 func Addition(a, b [][]int) ([][]int, error) {
 	c := make([][]int, len(a))
 
-	if len(a) == 0 || len(b) == 0 {
+	if checkInValidMatrices(a, b) {
 		return c, ErrInvalidMatrix
 	}
 
@@ -27,4 +27,37 @@ func Addition(a, b [][]int) ([][]int, error) {
 	}
 
 	return c, nil
+}
+
+func Multiplication(a, b [][]int) ([][]int, error) {
+	c := make([][]int, len(a))
+
+	if checkInValidMatrices(a, b) {
+		return c, ErrInvalidMatrix
+	}
+
+	// Columns of matrix A should be same as rows of matrix B
+	if len(a[0]) != len(b) {
+		return c, ErrMatricsMismatch
+	}
+
+	for i := range a {
+		c[i] = make([]int, len(b[0]))
+
+		for j := range b {
+			c[i][j] = 0
+
+			for k := range b {
+				c[i][j] += a[i][k] * b[k][j]
+			}
+		}
+	}
+
+	return c, nil
+}
+
+// --- Helper method --- //
+
+func checkInValidMatrices(a, b [][]int) bool {
+	return len(a) == 0 || len(b) == 0
 }
